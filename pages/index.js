c1203363d8fc33e222da69dcfa7ec56c88295ed2
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-12-12 18:16:43 +0200

    feat: added Footer component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Header from "../components/SharedComponents/Header";
 import SideBar from "../components/SharedComponents/SideBar";
+import Footer from "../components/SharedComponents/Footer";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 border border-black">
       <Header />
       <SideBar />
+      <Footer />
     </div>
   );
 }
