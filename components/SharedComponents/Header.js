955d4f5e5a035c487b22b11bca454a622130c1de
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-12-12 17:51:20 +0200

    feat: added Header component

---
+++ components/SharedComponents/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+<nav class="bg-gray-800">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+
+      </div>
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex-shrink-0 flex items-center">
+        <img src="https://img.icons8.com/external-flatart-icons-outline-flatarticons/50/3B82F6/external-performance-business-flatart-icons-outline-flatarticons.png"/>
+        </div>
+
+      </div>
+      <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <h1 className="text-white">User Name</h1>
+
+        <div class="ml-3 relative">
+          <div>
+            <button type="button" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+              <span class="sr-only">Open user menu</span>
+              <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+</nav>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,7 @@
-import Head from 'next/head'
-import LoginForm from '../components/LoginForm'
-import EvalutionTable from '../components/evalutionTable'
+
+import Header from "../components/SharedComponents/Header"
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
-      <Head>
-        <title>Performance Evaluation System</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* <header>
-        <img className="h-24 min-h-full" src='https://bohatala.com/wp-content/uploads/2015/08/performance-management.jpg'></img>
-      </header>  */}
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <EvalutionTable/>
-      </main>
-      {/* <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <LoginForm/>
-      </main> */}
-
-      {/* <footer className="flex items-center justify-center w-full h-24 border-t">
-        &copy; Performance Evaluation System. All rights reserved.
-      </footer> */}
-    </div>
+    <Header/>
   )
 }
