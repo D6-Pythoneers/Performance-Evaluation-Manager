3b8b6f309fe1c5d265931901e65bb7f10df37d04
Odeh <abuzaid163@hotmail.com>
2021-12-14 00:15:47 +0200

    fix: async api calls - school module

--- pages/api/School.js
+++ pages/api/School.js
@@ -1,6 +1,33 @@
 // 201 Basics :D
 import axios from 'axios'
 import { useAuth } from '../../contexts/auth'
+
+const api_get = async (token, url) => {
+  let jwt = token
+  let config = {
+    headers: { Authorization: 'Bearer ' + jwt },
+    method: 'get',
+    baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
+    url: `/${url}/?format=json`
+  }
+  const response = await axios(config).catch((error) =>
+    console.log(error.message)
+  )
+  return response.data
+}
+// const api_post = async (token, url,id,data) => {
+//   let jwt = token
+//   let config = {
+//     headers: { Authorization: 'Bearer ' + jwt },
+//     method: 'get',
+//     baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
+//     url: `/${url}/${id}`
+//   }
+//   const response = await axios(config).catch((error) =>
+//     console.log(error.message)
+//   )
+//   return response.data
+// }
 export default class School {
   constructor(...user) {
     const { tokens } = useAuth()
@@ -17,111 +44,56 @@ export default class School {
     this.getGoals()
   }
 
-  getShool() {
-    let jwt = this.tokens.access
-    let config = {
-      headers: { Authorization: 'Bearer ' + jwt },
-      method: 'get',
-      baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
-      url: '/schools/?format=json'
-    }
-    axios(config)
-      .then((response) => {
-        const schools = response.data
-        schools.forEach((item) => {
-          if (item.school_id == this.manager.sid) {
-            this.schoolInfo.push(item)
-          }
-        })
-      })
-      .catch((error) => console.log(error.message))
+  async getShool() {
+    const schools = await api_get(this.tokens.access, 'schools')
+    schools.forEach((item) => {
+      if (item.school_id == this.manager.sid) {
+        this.schoolInfo.push(item)
+      }
+    })
   }
-  getTeachers() {
-    let jwt = this.tokens.access
-    let config = {
-      headers: { Authorization: 'Bearer ' + jwt },
-      method: 'get',
-      baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
-      url: '/teachers/?format=json'
-    }
-    axios(config)
-      .then((response) => {
-        const teachers = response.data
-        teachers.forEach((item) => {
-          if (item.school_id == this.manager.sid) {
-            this.schoolTeachers.push(item)
-          }
-        })
-      })
-      .catch((error) => console.log(error.message))
+  async getTeachers() {
+    const teachers = await api_get(this.tokens.access, 'teachers')
+    teachers.forEach((item) => {
+      if (item.school_id == this.manager.sid) {
+        this.schoolTeachers.push(item)
+      }
+    })
   }
-  getEvaluations() {
-    let jwt = this.tokens.access
-    let config = {
-      headers: { Authorization: 'Bearer ' + jwt },
-      method: 'get',
-      baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
-      url: '/evaluations/?format=json'
-    }
-    axios(config)
-      .then((response) => {
-        const evaluations = response.data
-        evaluations.forEach((item) => {
-          if (
-            item.school == this.manager.sid &&
-            item.academic_year == this.academic_year
-          ) {
-            this.schoolEvaluations.push(item)
-          }
-        })
-      })
-      .catch((error) => console.log(error.message))
+  async getEvaluations() {
+    const evaluations = await api_get(this.tokens.access, 'evaluations')
+    evaluations.forEach((item) => {
+      if (
+        item.school == this.manager.sid &&
+        item.academic_year == this.academic_year
+      ) {
+        this.schoolEvaluations.push(item)
+      }
+    })
   }
-  getAssesments() {
-    let jwt = this.tokens.access
-    let config = {
-      headers: { Authorization: 'Bearer ' + jwt },
-      method: 'get',
-      baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
-      url: '/assesments/?format=json'
-    }
-    axios(config)
-      .then((response) => {
-        const assesments = []
-        this.schoolEvaluations.forEach((eva) => {
-          response.data.forEach((assesment) => {
-            if (eva.evaluation_id == assesment.evaluation) {
-              //add each assesment to the eavaluation
-              assesments.push(assesment)
-            }
-          })
-          eva['assesments'] = assesments
-        })
+  async getAssesments() {
+    const evaluations = await api_get(this.tokens.access, 'assesments')
+    const assesments = []
+    this.schoolEvaluations.forEach((eva) => {
+      evaluations.forEach((assesment) => {
+        if (eva.evaluation_id == assesment.evaluation) {
+          //add each assesment to the eavaluation
+          assesments.push(assesment)
+        }
       })
-
-      .catch((error) => console.log(error.message))
+      eva['assesments'] = assesments
+    })
   }
-  getGoals() {
-    let jwt = this.tokens.access
-    let config = {
-      headers: { Authorization: 'Bearer ' + jwt },
-      method: 'get',
-      baseURL: process.env.NEXT_PUBLIC_RESOURCE_URL,
-      url: '/goals/?format=json'
-    }
-    axios(config)
-      .then((response) => {
-        const goals = []
-        this.schoolEvaluations.forEach((eva) => {
-          response.data.forEach((goal) => {
-            if (eva.evaluation_id == goal.evaluation) {
-              goals.push(goal)
-            }
-          })
-          eva['goals'] = goals
-        })
+  async getGoals() {
+    const goals = []
+    const response = await api_get(this.tokens.access, 'goals')
+    this.schoolEvaluations.forEach((eva) => {
+      response.forEach((goal) => {
+        if (eva.evaluation_id == goal.evaluation) {
+          goals.push(goal)
+        }
       })
-
-      .catch((error) => console.log(error.message))
+      eva['goals'] = goals
+    })
   }
 }
