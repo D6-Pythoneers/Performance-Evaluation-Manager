5af278e02a241eee69deeb3e5515b003d6c03540
majedalswaeer <majedalswaeer69@gmail.com>
2021-12-14 18:52:05 +0200

    Style:Teachers cards created on the manager dashboard

--- components/ManagerDashboard/ManagerDashboard.js
+++ components/ManagerDashboard/ManagerDashboard.js
@@ -47,19 +47,34 @@ export default function ManagerDashboard() {
           </div>
         </div>
       </div>
+      <h2 className='ml-12 text-3xl mt-12'>Teachers</h2>
 
-      {/*  */}
-      <div className='flex flex-col w-2/5 mt-12 '>
-        <h2 className='ml-12 text-3xl'>Teachers</h2>
-        <div className='flex justify-between m-12 mt-4 overflow-x-hidden overflow-y-scroll w-fit h-96'>
-          <table className='w-96'>
-            <tbody className='bg-white'>
-              {resources
-                ? resources.map((teacher) => (<tr>{teacher.name}</tr>))
-                : false}
-            </tbody>
-          </table>
+      <div id="container" class="w-4/5 mx-auto ml-0">
+        <div className=" grid  grid-cols-3 gap-3 m-12 w-full ml-0 ">
+          {resources
+            ? resources.map((teacher) => (
+              <div class="bg-gray-800 px-6 py-8 rounded-lg shadow-lg text-center m-16 mt-0 mx-24 w-56 border border-gray">
+                <div class="mb-3">
+                  <img
+                    class="w-auto mx-auto rounded-full"
+                    src="https://thumbs.dreamstime.com/z/vector-illustration-isolated-white-background-user-profile-avatar-black-line-icon-user-profile-avatar-black-line-icon-121102131.jpg"
+                    alt=""
+                  />
+                </div>
+                <h2 class="text-xl font-medium text-white">{teacher.name}</h2>
+                <span class="text-white block mb-5">{teacher.role}</span>
+
+                <a href="#" class="px-4 py-2 bg-blue-800 text-white rounded-full"
+                >Evaluate</a>
+
+              </div>
+            ))
+            : false}
         </div>
+
+
+
+
       </div>
     </div>
   )
