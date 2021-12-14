e580dfe0b48d8942fb924c5c845f6a727d7b40ca
renadalkhlafat <renadsalem8888@gmail.com>
2021-12-14 02:13:09 +0200

    feat: user profile added

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Header() {
+export default function Header({user}) {
   return (
     <nav class="bg-gray-800">
       <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -12,7 +12,7 @@ export default function Header() {
             </div>
           </div>
           <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <h1 className="text-white">User Name</h1>
+            <h1 className="text-white uppercase">{user.name}</h1>
 
             <div class="ml-3 relative">
               <div>
@@ -23,11 +23,7 @@ export default function Header() {
                   aria-expanded="false"
                   aria-haspopup="true"
                 >
-                  <img
-                    class="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
+                  <img alt="..." class="w-8 h-8 mx-auto rounded-full" src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" />
                 </button>
               </div>
             </div>
--- components/ManagerDashboard/ManagerDashboard.js
+++ components/ManagerDashboard/ManagerDashboard.js
@@ -1,34 +1,40 @@
 import React from "react";
+import { useEffect, useState } from 'react'
+import { useAuth } from '../../contexts/auth'
+
 import TeacherRow from "./TeacherRow";
-export default function ManagerDashboard({school}) {
-  const {schoolTeachers,academic_year} = school
-  // console.log(schoolTeachers.length)
+import School from '../../pages/api/School'
+export default function ManagerDashboard() {
+  const { user } = useAuth()
+  const [school, SetSchool] = useState(new School(user))
+
+  console.log(school)
   return (
     <div className="w-full p-8">
       <div className="flex justify-around w-full">
         <div className="flex p-4 bg-white rounded-xl">
-          <img alt= "Academic Year" src="https://img.icons8.com/color/48/000000/calendar--v1.png" />
+          <img alt="Academic Year" src="https://img.icons8.com/color/48/000000/calendar--v1.png" />
           <div class="mx-5 bg-white min-w-max">
             <div class="text-gray-500">Academic Year</div>
-            <h4 class="text-2xl font-semibold text-gray-700">{academic_year}</h4>
+            <h4 class="text-2xl font-semibold text-gray-700">{school.academic_year}</h4>
           </div>
         </div>
         <div className="flex gap-4 p-4 bg-white rounded-xl">
-          <img alt= "Evaluation Phase" src="https://img.icons8.com/fluency/48/000000/test.png" />
+          <img alt="Evaluation Phase" src="https://img.icons8.com/fluency/48/000000/test.png" />
           <div class="mx-5 bg-white min-w-max">
             <div class="text-gray-500">Evaluation Phase</div>
             <h4 class="text-2xl font-semibold text-gray-700">First</h4>
           </div>
         </div>
         <div className="flex p-4 bg-white rounded-xl">
-          <img alt= "Pending Evaluations" src="https://img.icons8.com/color/48/000000/data-pending.png" />
+          <img alt="Pending Evaluations" src="https://img.icons8.com/color/48/000000/data-pending.png" />
           <div class="mx-5 bg-white">
             <div class="text-gray-500">Pending Evaluations</div>
             <h4 class="text-2xl font-semibold text-gray-700">10</h4>
           </div>
         </div>
         <div className="flex p-4 bg-white rounded-xl">
-          <img alt= "Completed Evaluations" src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-chart-data-analytics-icongeek26-linear-colour-icongeek26-1.png" />
+          <img alt="Completed Evaluations" src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-chart-data-analytics-icongeek26-linear-colour-icongeek26-1.png" />
           <div class="mx-5 bg-white">
             <div class="text-gray-500">Completed Evaluations</div>
             <h4 class="text-2xl font-semibold text-gray-700">15</h4>
@@ -39,13 +45,12 @@ export default function ManagerDashboard({school}) {
       {/*  */}
       <div className="flex flex-col w-2/5 mt-12 ">
         <h2 className="ml-12 text-3xl">Teachers</h2>
-
         <div className="flex justify-between m-12 mt-4 overflow-x-hidden overflow-y-scroll w-fit h-96">
           <table class="w-96">
             <tbody class="bg-white">
-              {schoolTeachers.map((itm, i) => (
-                <TeacherRow name={itm} key={i} />
-              ))}
+              {
+                 school.schoolTeachers.map(teacher => console.log(teacher))
+                 }
             </tbody>
           </table>
         </div>
