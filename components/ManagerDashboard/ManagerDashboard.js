277c46cf33c9c6ab87232e13db28b3254c8f7372
Odeh <abuzaid163@hotmail.com>
2021-12-14 16:31:31 +0200

    fix:typos and added eslint configs

--- components/ManagerDashboard/ManagerDashboard.js
+++ components/ManagerDashboard/ManagerDashboard.js
@@ -1,9 +1,7 @@
-import React from "react";
-import { useEffect, useState } from 'react'
-import { useAuth } from '../../contexts/auth'
+import React, { useState } from "react";
+import { useAuth } from '../../contexts/auth';
+import School from '../../pages/api/School';
 
-import TeacherRow from "./TeacherRow";
-import School from '../../pages/api/School'
 export default function ManagerDashboard() {
   const { user } = useAuth()
   const [school, SetSchool] = useState(new School(user))
@@ -14,30 +12,30 @@ export default function ManagerDashboard() {
       <div className="flex justify-around w-full">
         <div className="flex p-4 bg-white rounded-xl">
           <img alt="Academic Year" src="https://img.icons8.com/color/48/000000/calendar--v1.png" />
-          <div class="mx-5 bg-white min-w-max">
-            <div class="text-gray-500">Academic Year</div>
-            <h4 class="text-2xl font-semibold text-gray-700">{school.academic_year}</h4>
+          <div className="mx-5 bg-white min-w-max">
+            <div className="text-gray-500">Academic Year</div>
+            <h4 className="text-2xl font-semibold text-gray-700">{school.academic_year}</h4>
           </div>
         </div>
         <div className="flex gap-4 p-4 bg-white rounded-xl">
           <img alt="Evaluation Phase" src="https://img.icons8.com/fluency/48/000000/test.png" />
-          <div class="mx-5 bg-white min-w-max">
-            <div class="text-gray-500">Evaluation Phase</div>
-            <h4 class="text-2xl font-semibold text-gray-700">First</h4>
+          <div className="mx-5 bg-white min-w-max">
+            <div className="text-gray-500">Evaluation Phase</div>
+            <h4 className="text-2xl font-semibold text-gray-700">First</h4>
           </div>
         </div>
         <div className="flex p-4 bg-white rounded-xl">
           <img alt="Pending Evaluations" src="https://img.icons8.com/color/48/000000/data-pending.png" />
-          <div class="mx-5 bg-white">
-            <div class="text-gray-500">Pending Evaluations</div>
-            <h4 class="text-2xl font-semibold text-gray-700">10</h4>
+          <div className="mx-5 bg-white">
+            <div className="text-gray-500">Pending Evaluations</div>
+            <h4 className="text-2xl font-semibold text-gray-700">10</h4>
           </div>
         </div>
         <div className="flex p-4 bg-white rounded-xl">
           <img alt="Completed Evaluations" src="https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-chart-data-analytics-icongeek26-linear-colour-icongeek26-1.png" />
-          <div class="mx-5 bg-white">
-            <div class="text-gray-500">Completed Evaluations</div>
-            <h4 class="text-2xl font-semibold text-gray-700">15</h4>
+          <div className="mx-5 bg-white">
+            <div className="text-gray-500">Completed Evaluations</div>
+            <h4 className="text-2xl font-semibold text-gray-700">15</h4>
           </div>
         </div>
       </div>
@@ -46,8 +44,8 @@ export default function ManagerDashboard() {
       <div className="flex flex-col w-2/5 mt-12 ">
         <h2 className="ml-12 text-3xl">Teachers</h2>
         <div className="flex justify-between m-12 mt-4 overflow-x-hidden overflow-y-scroll w-fit h-96">
-          <table class="w-96">
-            <tbody class="bg-white">
+          <table className="w-96">
+            <tbody className="bg-white">
               {
                  school.schoolTeachers.map(teacher => console.log(teacher))
                  }
--- pages/profile.js
+++ pages/profile.js
@@ -1,26 +1,26 @@
 import React from "react";
 export default function ManagerProfile({ user }) {
     return (
-        <div class=" flex flex-col max-w-md mx-auto bg-gray-200 opacity-100 font-poppins px-4 bg-no-repeat bg-cover bg-center">
-            <div class="w-80 h-80 bg-blue-600 flex items-center rounded-full">
-                    <img alt="..." class="w-80 h-80 mx-auto rounded-full" src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" />
+        <div className=" flex flex-col max-w-md mx-auto bg-gray-200 opacity-100 font-poppins px-4 bg-no-repeat bg-cover bg-center">
+            <div className="w-80 h-80 bg-blue-600 flex items-center rounded-full">
+                    <img alt="..." className="w-80 h-80 mx-auto rounded-full" src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" />
                 </div>
-                <div class="flex items-center px-4 pt-12 justify-between">
+                <div className="flex items-center px-4 pt-12 justify-between">
                 
-                <div class=" flex items-center">
-                    <div class=" flex flex-col leading-none pl-4">
-                        <h3 class="text-2xl font-bold uppercase w-full">Name :{user.name}</h3>
-                        <p class="text-xl font-semibold text-gray-600 mt-3">Email : {user.email}</p>
-                        <p class="text-lg pt-1 font-light text-gray-700">Role : {user.role}</p>
+                <div className=" flex items-center">
+                    <div className=" flex flex-col leading-none pl-4">
+                        <h3 className="text-2xl font-bold uppercase w-full">Name :{user.name}</h3>
+                        <p className="text-xl font-semibold text-gray-600 mt-3">Email : {user.email}</p>
+                        <p className="text-lg pt-1 font-light text-gray-700">Role : {user.role}</p>
                     </div>
                 </div>
             </div>
-            <div class="pt-12 px-4 w-full flex flex-col">
+            <div className="pt-12 px-4 w-full flex flex-col">
                 
                 
             </div>
-            <div class="pt-12 px-4 w-full flex flex-col">
-                <p class="font-semibold text-gray-600">{user.school}</p>
+            <div className="pt-12 px-4 w-full flex flex-col">
+                <p className="font-semibold text-gray-600">{user.school}</p>
 
             </div>
         </div>
