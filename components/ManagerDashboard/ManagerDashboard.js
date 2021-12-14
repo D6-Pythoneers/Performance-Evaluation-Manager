ebf5ac39b51d36cf5d764fdd5a3815b7d6474dea
Odeh <abuzaid163@hotmail.com>
2021-12-14 21:01:05 +0200

    fix: school data api-calls async & conditional rendering

--- components/ManagerDashboard/ManagerDashboard.js
+++ components/ManagerDashboard/ManagerDashboard.js
@@ -1,66 +1,64 @@
 import React from 'react'
 import useResouce from '../../hooks/useResource'
-export default function ManagerDashboard() {
-  const { resources } = useResouce()
+import { createContext, useContext, useState, useEffect } from "react";
+import { render } from 'react-dom';
+export default function ManagerDashboard ({ resources }) {
   console.log(resources)
   return (
-    <div className='w-full p-8'>
-      <div className='flex justify-around w-full'>
-        <div className='flex p-4 bg-white rounded-xl'>
-          <img
-            alt='Academic Year'
-            src='https://img.icons8.com/color/48/000000/calendar--v1.png'
-          />
-          <div className='mx-5 bg-white min-w-max'>
-            <div className='text-gray-500'>Academic Year</div>
-            <h4 className='text-2xl font-semibold text-gray-700'>2021</h4>
+    resources != null ?
+      (<div className='w-full p-8'>
+        <div className='flex justify-around w-full'>
+          <div className='flex p-4 bg-white rounded-xl'>
+            <img
+              alt='Academic Year'
+              src='https://img.icons8.com/color/48/000000/calendar--v1.png'
+            />
+            <div className='mx-5 bg-white min-w-max'>
+              <div className='text-gray-500'>Academic Year</div>
+              <h4 className='text-2xl font-semibold text-gray-700'>2021</h4>
+            </div>
+          </div>
+          <div className='flex gap-4 p-4 bg-white rounded-xl'>
+            <img
+              alt='Evaluation Phase'
+              src='https://img.icons8.com/fluency/48/000000/test.png'
+            />
+            <div className='mx-5 bg-white min-w-max'>
+              <div className='text-gray-500'>Evaluation Phase</div>
+              <h4 className='text-2xl font-semibold text-gray-700'>First</h4>
+            </div>
+          </div>
+          <div className='flex p-4 bg-white rounded-xl'>
+            <img
+              alt='Pending Evaluations'
+              src='https://img.icons8.com/color/48/000000/data-pending.png'
+            />
+            <div className='mx-5 bg-white'>
+              <div className='text-gray-500'>Pending Evaluations</div>
+              <h4 className='text-2xl font-semibold text-gray-700'>10</h4>
+            </div>
+          </div>
+          <div className='flex p-4 bg-white rounded-xl'>
+            <img
+              alt='Completed Evaluations'
+              src='https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-chart-data-analytics-icongeek26-linear-colour-icongeek26-1.png'
+            />
+            <div className='mx-5 bg-white'>
+              <div className='text-gray-500'>Completed Evaluations</div>
+              <h4 className='text-2xl font-semibold text-gray-700'>15</h4>
+            </div>
           </div>
         </div>
-        <div className='flex gap-4 p-4 bg-white rounded-xl'>
-          <img
-            alt='Evaluation Phase'
-            src='https://img.icons8.com/fluency/48/000000/test.png'
-          />
-          <div className='mx-5 bg-white min-w-max'>
-            <div className='text-gray-500'>Evaluation Phase</div>
-            <h4 className='text-2xl font-semibold text-gray-700'>First</h4>
-          </div>
-        </div>
-        <div className='flex p-4 bg-white rounded-xl'>
-          <img
-            alt='Pending Evaluations'
-            src='https://img.icons8.com/color/48/000000/data-pending.png'
-          />
-          <div className='mx-5 bg-white'>
-            <div className='text-gray-500'>Pending Evaluations</div>
-            <h4 className='text-2xl font-semibold text-gray-700'>10</h4>
-          </div>
-        </div>
-        <div className='flex p-4 bg-white rounded-xl'>
-          <img
-            alt='Completed Evaluations'
-            src='https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-chart-data-analytics-icongeek26-linear-colour-icongeek26-1.png'
-          />
-          <div className='mx-5 bg-white'>
-            <div className='text-gray-500'>Completed Evaluations</div>
-            <h4 className='text-2xl font-semibold text-gray-700'>15</h4>
-          </div>
-        </div>
-      </div>
 
-      {/*  */}
-      <div className='flex flex-col w-2/5 mt-12 '>
-        <h2 className='ml-12 text-3xl'>Teachers</h2>
-        <div className='flex justify-between m-12 mt-4 overflow-x-hidden overflow-y-scroll w-fit h-96'>
-          <table className='w-96'>
-            <tbody className='bg-white'>
-              {resources
-                ? resources.map((teacher) => (<tr>{teacher.name}</tr>))
-                : false}
-            </tbody>
-          </table>
+        {/*  */}
+        <div className='flex flex-col w-2/5 mt-12 '>
+          <h2 className='ml-12 text-3xl'>Teachers</h2>
+          <div className='flex justify-between m-12 mt-4 w-fit h-96'>
+            {resources.schoolTeachers.map((teacher, index) => {
+              return (<div key={index}>{teacher.name}</div>)
+            })}
+          </div>
         </div>
-      </div>
-    </div>
+      </div>) : null
   )
 }
--- hooks/useResource.js
+++ hooks/useResource.js
@@ -3,54 +3,100 @@ import useSWR from 'swr'
 // import { useAuth } from '../contexts/auth'
 
 export const apiUrl = process.env.NEXT_PUBLIC_RESOURCE_URL
-
-export default function useResource() {
-  //   const { logout } = useAuth()
+import School from '../pages/api/School'
+export default function useResource (user) {
+  // const { user } = useAuth()
   const tokens = JSON.parse(window.localStorage.getItem('tokens'))
   const { data, error, mutate } = useSWR([apiUrl, tokens], fetchResource)
-  
-  async function fetchResource(url) {
+
+  async function fetchResource (url) {
     if (!tokens) {
       return
     }
-    
-    try {
-      const response = await axios.get(`${url}teachers`, config())
 
-      return response.data
+    try {
+      let school = {
+        academic_year: new Date().getFullYear().toString(),
+        manager: user,
+        schoolInfo: [],
+        schoolTeachers: [],
+        schoolEvaluations: [],
+        academic_year: new Date().getFullYear().toString()
+      }
+      await axios.get(`${url}schools/`, config()).then(
+        response => {
+          let sc = []
+          response.data.forEach((item) => {
+            if (item.school_id == school.manager.sid) {
+              sc.push(item)
+            }
+          })
+          school.schoolInfo = sc
+
+        }
+      ).catch(err => console.log(err))
+
+      await axios.get(`${url}teachers/`, config()).then(
+        response => {
+          let st = []
+          response.data.forEach((item) => {
+            if (item.school_id == school.manager.sid) {
+              st.push(item)
+            }
+          })
+          school.schoolTeachers = st
+        }
+      ).catch(err => console.log(err))
+
+      await axios.get(`${url}evaluations/`, config()).then(
+        response => {
+          let schoolEvaluation = []
+          response.data.forEach((item) => {
+            if (
+              item.school == school.manager.sid &&
+              item.academic_year == school.academic_year
+            ) {
+              schoolEvaluation.push(item)
+            }
+          })
+          school.schoolEvaluations = schoolEvaluation
+        }
+      ).catch(err => console.log(err))
+
+
+      await axios.get(`${url}goals/`, config()).then(response => {
+        const goals = []
+        school.schoolEvaluations.forEach((eva) => {
+          response.data.forEach((goal) => {
+            if (eva.evaluation_id == goal.evaluation) {
+              goals.push(goal)
+            }
+          })
+          eva['goals'] = goals
+        })
+      }).catch(err => console.log(err))
+
+      await axios.get(`${url}assesments/`, config()).then(response => {
+        const assesments = []
+        school.schoolEvaluations.forEach((eva) => {
+          response.data.forEach((assesment) => {
+            if (eva.evaluation_id == assesment.evaluation) {
+              assesments.push(assesment)
+            }
+          })
+          eva['assesments'] = assesments
+        })
+      }).catch(err => console.log(err))
+      
+      console.log('ress ->>>>', school)
+      return school
     } catch (error) {
       handleError(error)
     }
   }
 
-  // async function createResource(info) {
-
-  //     try {
-  //         const res = await axios.post(apiUrl, info, config());
-  //         mutate(); // mutate causes complete collection to be refetched
-  //     } catch (error) {
-  //         handleError(error);
-  //     }
-  // }
-
-  // async function deleteResource(id) {
-
-  //     try {
-  //         const url = apiUrl + id;
-  //         await axios.delete(url, config());
-  //         mutate(); // mutate causes complete collection to be refetched
-  //     } catch (error) {
-  //         handleError(error);
-  //     }
-  // }
-
-  // async function updateResource(resource) {
-  //     // STRETCH
-  //     // Add ability for user to update an existing resource
-  // }
-
   // helper function to handle getting Authorization headers EXACTLY right
-  function config() {
+  function config () {
     return {
       headers: {
         'Authorization': 'Bearer ' + tokens.access
@@ -58,7 +104,7 @@ export default function useResource() {
     }
   }
 
-  function handleError(error) {
+  function handleError (error) {
     console.error(error)
     // currently just log out on error
     // but a common error will be short lived token expiring
